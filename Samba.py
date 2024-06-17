00547a70df70db53be77780460bb34c091628c64
Phlapa <phmsllapa@gmail.com>
2024-06-17 11:58:55 -0300

    Update Samba.py

--- Samba.py
+++ Samba.py
@@ -1,4 +1,5 @@
 import pandas as pd
+url='https://pt.wikipedia.org/wiki/Lista_de_campe%C3%A3s_do_carnaval_do_Rio_de_Janeiro'
 df = pd.read_html(url, match='Títulos')[1]
 df = df.drop(columns=['#', 'Escola de samba', 'Escola de samba.1']).rename(columns={'Escola de samba.2': 'Escola de Samba'})
 
