269d6946784789b122a66df5c8cd7d37c3f9410b
Phlapa <phmsllapa@gmail.com>
2024-06-17 12:09:53 -0300

    Update Samba.py

--- Samba.py
+++ Samba.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 url='https://pt.wikipedia.org/wiki/Lista_de_campe%C3%A3s_do_carnaval_do_Rio_de_Janeiro'
 
@@ -11,5 +12,7 @@ st.header('As maiores campeãs do carnaval carioca')
 
 st.write(df)
 
-grafico = sns.barplot(data=df, x='Títulos', y='Escola de Samba')
+grafico = plt.plot(df['Títulos'], df['Escola de Samba'])
 st.write(grafico)
+
+
