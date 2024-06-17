cc2190078bfc1b00627925117d707968d0cb77eb
Phlapa <phmsllapa@gmail.com>
2024-06-17 12:07:41 -0300

    Update Samba.py

--- Samba.py
+++ Samba.py
@@ -4,4 +4,5 @@ df = pd.read_html(url, match='Títulos')[1]
 df = df.drop(columns=['#', 'Escola de samba', 'Escola de samba.1']).rename(columns={'Escola de samba.2': 'Escola de Samba'})
 
 import seaborn as sns
-sns.barplot(data=df, x='Títulos', y='Escola de Samba')
+grafico = sns.barplot(data=df, x='Títulos', y='Escola de Samba')
+st.write(grafico)
