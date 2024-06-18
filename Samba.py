d7f3d644ccfe3b3a93fccbcc369fe07e92923949
joaovictorvasco <168442928+joaovictorvasco@users.noreply.github.com>
2024-06-17 22:29:40 -0300

    Update Samba.py

--- Samba.py
+++ Samba.py
@@ -5,18 +5,10 @@ import matplotlib.pyplot as plt
 url = 'https://pt.wikipedia.org/wiki/Lista_de_campe%C3%A3s_do_carnaval_do_Rio_de_Janeiro'
 
 # Carregar a tabela correta da página da Wikipedia
-dfs = pd.read_html(url)
-# Inspecionar todas as tabelas para encontrar a correta
-for i, df in enumerate(dfs):
-    st.write(f"Tabela {i}")
-    st.write(df.head())
-
-# Baseado na inspeção, carregar a tabela correta
-# Vamos assumir que a tabela correta é a que contém a coluna 'Títulos'
-df = pd.read_html(url, match='Títulos')[1]
+df = pd.read_html(url)[4]
 
 # Limpar e transformar os dados
-df = df.drop(columns=['#', 'Escola de samba', 'Escola de samba.1']).rename(columns={'Escola de samba.2': 'Escola de Samba'})
+df = df[['Escola de samba.2', 'Títulos', 'Anos']].rename(columns={'Escola de samba.2': 'Escola de Samba'})
 
 # Mostrar as primeiras linhas para garantir que a tabela está correta
 st.write("Dados após limpeza e renomeação:")
